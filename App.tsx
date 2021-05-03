1838040d5ae470282f3881e2d151e84a7a35aad2
tadashi <tadashi.b@gmail.com>
2021-05-02 22:03:42 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications';
+
+import Routes from './src/routes';
+import { PlantsProps } from './src/libs/storage';
+
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
+
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+  useEffect(() => {
+    //Resolve o problema para aparecer as notificações enquanto o app está aberto
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantsProps;
+        console.log(data);
+      });
+      return () => subscription.remove();
+  
+    // async function notifications() { 
+    // await Notifications.cancelAllScheduledNotificationsAsync();
+
+    // const data = await Notifications.getAllScheduledNotificationsAsync();
+    // console.log('===NOTIFICAÇÕES AGENDADAS===');
+    // console.log(data);
+    // }
+
+    // notifications();
+    
+
+  }, []);
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
